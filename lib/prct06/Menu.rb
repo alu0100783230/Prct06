7b897f691cb39a3b00cb5ad52c76f2141541d971
Daniel Darias <dariasteam94@gmail.com>
2016-11-04 11:50:27 +0000

    Prueba satisfecha, P2

--- lib/prct06/Menu.rb
+++ lib/prct06/Menu.rb
@@ -1,10 +1,11 @@
 class Menu
-  attr_reader :Title, :Porcentage, :Content
+  attr_reader :Title, :Porcentage, :Content, :vct
 
   def initialize(title, porcentage, content)
     @Title = check_name title
     @Porcentage = check_porcentage porcentage
     @Content = check_content content
+    @vct = get_vct
   end
 
   def check_name(title)
@@ -27,17 +28,25 @@ class Menu
 
   def check_content(content)
     if (!content.kind_of?(Array))
-      content = [Plate.new("Croqueta","1",20,nil)]
+      content = [Plate.new("Croqueta","1",20,Nutrition_Info.new(10, 15, 20, 25))]
     else
       content.map! do |x|
         if(!x.kind_of?(Plate))
-          Plate.new("Croqueta","1",20,nil)
+          Plate.new("Croqueta","1",20,Nutrition_Info.new(10, 15, 20, 25))
         else
           x
         end
       end
     end
     content
+  end
+
+  def get_vct
+    vct = 0
+    @Content.each do |x|
+      vct +=  x.NutritionalInfo.Calories
+    end
+    vct
   end
 
 end
