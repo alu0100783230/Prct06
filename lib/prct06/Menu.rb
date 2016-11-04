f020a9a057e535f3b436f91f5753cf1d1ec6da17
Daniel Darias <dariasteam94@gmail.com>
2016-11-04 11:02:54 +0000

    Prueba satisfecha, P2

--- lib/prct06/Menu.rb
+++ lib/prct06/Menu.rb
@@ -1,9 +1,10 @@
 class Menu
-  attr_reader :Title, :Porcentage
+  attr_reader :Title, :Porcentage, :Content
 
-  def initialize(title, porcentage)
+  def initialize(title, porcentage, content)
     @Title = check_name title
     @Porcentage = check_porcentage porcentage
+    @Content = check_content content
   end
 
   def check_name(title)
@@ -22,6 +23,19 @@ class Menu
     else
       10
     end
+  end
+
+  def check_content(content)
+    if (!content.kind_of?(Array))
+      content = [Plate.new("Croqueta","1",20)]
+    else
+      content.each do |x|
+        if !x.kind_of?(Plate)
+          x = Plate.new("Croqueta","1",20)
+        end
+      end
+    end
+    content
   end
 
 end
