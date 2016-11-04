436821342ef57f1789d2671f33b8e0c1b269e996
Daniel Darias <dariasteam94@gmail.com>
2016-11-04 10:08:48 +0000

    Mejorada la prueba anterior, por defecto los menus son desayuno, P1

--- spec/prct06_spec.rb
+++ spec/prct06_spec.rb
@@ -3,16 +3,29 @@ require "spec_helper"
 describe Prct06 do
 
   before :all do
-    @title = "desayuno media manana almuerzo merienda cena"
+    @menu_good_1 = Menu.new ("Desayuno")
+    @menu_good_2 = Menu.new ("Almuerzo")
+    @menu_good_3 = Menu.new ("Cena")
+    @menu_good_4 = Menu.new ("Media Mañana")
+    @menu_good_5 = Menu.new ("Merienda")
 
-    @menu = Menu.new (@title)
+    @menu_fail = Menu.new ("Anything")
   end
 
   it "has a version number" do
     expect(Prct06::VERSION).not_to be nil
   end
 
-  it "a title must exist" do
-    expect(@menu.Title).to eq(@title)
+  it "The title must be on of these" do
+    expect(@menu_good_1.Title).to eq("Desayuno")
+    expect(@menu_good_2.Title).to eq("Almuerzo")
+    expect(@menu_good_3.Title).to eq("Cena")
+    expect(@menu_good_4.Title).to eq("Media Mañana")
+    expect(@menu_good_5.Title).to eq("Merienda")
   end
+
+  it "The title can't be anything" do
+    expect(@menu.Title).to eq("Desayuno")
+  end
+
 end
